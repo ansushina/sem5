276b706c3959ed9935e669d08965d6aaf79e3fda
ansushina <nastya.sushina@yandex.ru>
2019-10-19 00:59:40 +0300

    trying to do laba

--- database/addData.go
+++ database/addData.go
@@ -5,14 +5,21 @@ import (
     "fmt"
     _ "github.com/lib/pq"
     faker "syreclabs.com/go/faker"
-    "reflect"
+    //"reflect"
     "os"
 
     "math/rand"
     //"bufio"
     "time"
-    "strconv"
+    //"strconv"
 )
+
+const labelcnt int = 300
+const artistcnt int = 700
+const composercnt int = 500
+const songscnt int = 1000
+const sacnt int = 1200
+const sccnt int = 1200
 
 type FileWriter struct {
 	filename string
@@ -35,11 +42,19 @@ func (f *FileWriter) Write(strSlice []string) error {
 }
 
 
+/*
+    LabelID int not null PRIMARY KEY ,
+    CraeteDate date,
+    Title varchar(30),
+    Country varchar(40),
+    Creator varchar(50)
+*/
+
 func create_labels(count int) []string {
     var strSlice []string
     for i := 0; i < count; i++ {
-        str := fmt.Sprintf("%s, %s, %s", 
-            
+        str := fmt.Sprintf("%d;%s;%s;%s", 
+            i+1000,
             faker.Company().Name(), 
             faker.Address().Country(), 
             faker.Name().Name());
@@ -54,51 +69,83 @@ func create_artists(count int) []string {
     
     for i := 0; i < count; i++ {
         age := faker.Number().Between(15,90)
-        ageNum,_ := strconv.Atoi(age)
         sexNum := faker.Number().Between(0,1)
+        label := faker.Number().Between(1000, labelcnt+1000 -1)
         sex := "male"
         if (sexNum == "1"){
             sex = "female"
         }
 
-        str := fmt.Sprintf("%s, %s, %s, %s, %s, %s", 
+        str := fmt.Sprintf("%d;%s;%s;%s;%s;%s;%s", 
+            i+1000,
             age,
             faker.Address().Country(),
-            faker.Date().Birthday(0, ageNum), 
             faker.Name().FirstName(),
             faker.Name().LastName(),
-            sex)
+            sex, 
+            label)
         strSlice = append(strSlice, str)
     }
 
     return strSlice
 }
 
-func create_composers(count int) []string {
+func create_mv(count int) []string {
     var strSlice []string
-    
     for i := 0; i < count; i++ {
-        age := faker.Number().Between(15,90)
-        ageNum,_ := strconv.Atoi(age)
-        sexNum := faker.Number().Between(0,1)
-        sex := "male"
-        if (sexNum == "1"){
-            sex = "female"
-        }
-
-        str := fmt.Sprintf("%s, %s, %s, %s, %s, %s", 
-            age,
-            faker.Address().Country(),
-            faker.Date().Birthday(0, ageNum), 
-            faker.Name().FirstName(),
-            faker.Name().LastName(),
-            sex)
+        str := fmt.Sprintf("%d;%s;%s", 
+            i+1000,
+            faker.Lorem().Word(),
+            faker.Name().Name());
         strSlice = append(strSlice, str)
     }
 
     return strSlice
 }
 
+func create_songs(count int) []string {
+    
+    var strSlice []string
+    for i := 0; i < count; i++ {
+        str := fmt.Sprintf("%d;%s;%d", 
+            i+1000,
+            faker.Lorem().Word(),
+            i+1000);
+        strSlice = append(strSlice, str)
+    }
+
+    return strSlice
+}
+
+func create_sa(songscnt int,artistcnt int) []string {
+    var strSlice []string
+    for i := 0; i < sacnt; i++ {
+        a := faker.Number().Between(1000, artistcnt+1000-1)
+        s := faker.Number().Between(1000, songscnt+1000-1)
+        str := fmt.Sprintf("%d;%s;%s", 
+            i+1000,
+            s,
+            a);
+        strSlice = append(strSlice, str)
+    }
+
+    return strSlice
+}
+
+func create_sc(songscnt int, composercnt int) []string {
+    var strSlice []string
+    for i := 0; i < sccnt; i++ {
+        c := faker.Number().Between(1000, composercnt+1000-1)
+        s := faker.Number().Between(1000, songscnt+1000-1)
+        str := fmt.Sprintf("%d;%s;%s", 
+            i+1000,
+            s,
+            c);
+        strSlice = append(strSlice, str)
+    }
+
+    return strSlice
+}
 
 func main() {
 
@@ -115,13 +162,37 @@ func main() {
     defer db.Close()
 
 
-    labels := create_labels(5)
-    writer := FileWriter{"labels.txt"}
+    labels := create_labels(labelcnt)
+    writer := FileWriter{"/tmp/labels.csv"}
 
     writer.Write(labels)
 
-    artists := create_artists(5)
-    fmt.Println(artists)
+    artists := create_artists(artistcnt)
+    writer = FileWriter{"/tmp/artist.csv"}
+    writer.Write(artists)
+
+    composers := create_artists(composercnt)
+    writer = FileWriter{"/tmp/composers.csv"}
+    writer.Write(composers)
+
+    mv := create_mv(songscnt)
+    writer = FileWriter{"/tmp/musicvideos.csv"}
+    writer.Write(mv)
+
+    songs := create_songs(songscnt)
+    writer = FileWriter{"/tmp/songs.csv"} 
+    writer.Write(songs)
+
+    sa := create_sa(songscnt,artistcnt)
+    writer = FileWriter{"/tmp/sa.csv"}
+    writer.Write(sa)
+
+    sc := create_sc(songscnt,composercnt)
+    writer = FileWriter{"/tmp/sc.csv"}
+    writer.Write(sc)
+
+    //fmt.Println(artists)
+
    
     /* 
     result, err := db.Exec("insert into dbo.Label (LabelID, CraeteDate, Title, Country, Creator) values ($1, $2, $3, $4, $5)", 
@@ -130,6 +201,6 @@ func main() {
         panic(err)
     }*/
 
-    fmt.Println(reflect.TypeOf(db))
-    fmt.Println(faker.Name().Name())
+    //fmt.Println(reflect.TypeOf(db))
+   // fmt.Println(faker.Name().Name())
 }
--- database/sql/constraint.sql
+++ database/sql/constraint.sql
@@ -1,38 +1,33 @@
-use MusicLabelsDB
-GO
 
 ALTER TABLE dbo.artist ADD
-CONSTRAINT artist_age_chk CHECK (age > 0)
-GO
+CONSTRAINT artist_age_chk CHECK (age > 0);
+
 ALTER TABLE dbo.composer ADD
-CONSTRAINT composer_age_chk CHECK (age > 0)
-GO
+CONSTRAINT composer_age_chk CHECK (age > 0);
 
 alter table dbo.Song add constraint FK_MV foreign key (musicvideoid) references dbo.musicvideo (MusicVideoID);
-GO
+ 
 alter table dbo.artist add constraint FK_LA foreign key (labelID) references dbo.label(labelid);
-go
+ 
 alter table dbo.composer add constraint FK_LC foreign key (labelID) references dbo.label(labelid);
-go
 
 create table dbo.SA (
     ID int not null primary key,
     SongID int not null,
     ArtistID int not null
 );
-GO
+ 
 create table dbo.SC (
     ID int not null primary key,
     SongID int not null,
     ComposerID int not null
 );
-GO
+ 
 alter table dbo.SA add constraint FK_SA foreign key (SongID) references dbo.song (SongID);
-Go
+ 
 alter table dbo.SA add constraint FK_artist foreign key (ArtistID) references dbo.artist (ArtistID);
-go
 
 alter table dbo.SC add constraint FK_SC foreign key (SongID) references dbo.song (SongID);
-go
+ 
 alter table dbo.SC add constraint FK_composer foreign key (ComposerID) references dbo.composer (ComposerID);
-go
+ 
